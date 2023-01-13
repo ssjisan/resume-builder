2106d21b2fbba3ade372e818f19cd770dbfa12a2
ssjisan <sakibfset13@gmail.com>
2023-01-14 01:58:30 +0600

    preview section added

--- src/Component/Preview/InstantPreview.js
+++ src/Component/Preview/InstantPreview.js
@@ -1,7 +1,7 @@
 import { Box, Button, Typography } from "@mui/material";
 import React from "react";
 import GridViewIcon from "@mui/icons-material/GridView";
-
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 export default function InstantPreview() {
   return (
     <Box
@@ -13,30 +13,50 @@ export default function InstantPreview() {
         bottom: "0px",
         right: "0px",
         width: "50%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
       }}
     >
-      <Typography sx={{ color: "#fff" }}>Instant Preview</Typography>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
-        }}
-      >
-        <Button sx={{ color: "#fff", textTransform: "none" }}>
-          {" "}
-          <GridViewIcon /> &nbsp; Select Category
-        </Button>
-        <Button
-          variant="outlined"
+      <Box sx={{ width: "585px", p:2}}>
+        <Typography sx={{ color: "#fff",mb:2 }}>Instant Preview</Typography>
+        <Box sx={{backgroundColor:"#fff", width:"100%",height:"800px"}}>s</Box>
+        <Box
           sx={{
-            color: "#fff",
-            textTransform: "none",
-            border: "1px solid #fff",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            mt:2
           }}
         >
-          Download Pdf
-        </Button>
+          <Button sx={{ color: "#fff", textTransform: "none" }}>
+            {" "}
+            <GridViewIcon /> &nbsp; Select Category
+          </Button>
+          <Box sx={{ display: "flex", gap: 2 }}>
+            <Button
+              variant="outlined"
+              sx={{
+                color: "#fff",
+                textTransform: "none",
+                border: "1px solid #fff",
+              }}
+            >
+              Download Pdf
+            </Button>
+            <Button
+              variant="outlined"
+              sx={{
+                color: "#fff",
+                textTransform: "none",
+                border: "1px solid #fff",
+                minWidth:"0px"
+              }}
+            >
+              <MoreVertIcon />
+            </Button>
+          </Box>
+        </Box>
       </Box>
     </Box>
   );
