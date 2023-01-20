fd691364698bf30204c76830cda89bd819afd9fd
ssjisan <sakibfset13@gmail.com>
2023-01-20 23:58:28 +0600

    remove functions

--- src/Component/Builder/ProfessionalSummary.js
+++ src/Component/Builder/ProfessionalSummary.js
@@ -1,26 +1,8 @@
 import { Box, Typography } from "@mui/material";
-import React, { useRef, useState } from "react";
+import React from "react";
 
 export default function ProfessionalSummary() {
-  const [isEditing, setIsEditing] = useState(false);
-  const inputRef = useRef(null);
-  const [inputValue, setInputValue] = useState('');
 
-  const handleEditClick = () => {
-    setIsEditing(true);
-    inputRef.current.disabled = false;
-    inputRef.current.focus();
-    inputRef.current.select();
-  };
-
-  const handleBlur = () => {
-    setIsEditing(false);
-    inputRef.current.disabled = true;
-  }
-  const handleChange = (event) => {
-    const title = event.target.value;
-    setInputValue(title);
-  };
   // useEffect(() => {
   //   function handleClickOutside(event) {
   //     if (inputRef.current && !inputRef.current.contains(event.target)) {
@@ -55,16 +37,6 @@ export default function ProfessionalSummary() {
         your role, experience & most importantly - your biggest achievements,
         best qualities and skills.
       </Typography>
-      <div>
-      <input 
-        ref={inputRef} 
-        value={inputValue===""? "Untitled" : inputValue} 
-        disabled={!isEditing}
-        onBlur={handleBlur}
-        onChange={(e)=>handleChange(e)}
-      />
-      <button onClick={handleEditClick}>Edit</button>
-    </div>
     </Box>
   );
 }
